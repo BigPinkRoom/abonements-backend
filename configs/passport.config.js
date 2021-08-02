3976e0e28ca15a991c2eb1bd883fe13a32708b38
Tester <TestTestTest1000@Test.com.ru>
2021-08-02 21:22:58 +0300

    change and add new logic in passport.js

--- configs/passport.config.js
+++ configs/passport.config.js
@@ -1,18 +1,21 @@
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../components/users/user');
+const service = require('../components/users/usersService');
 const userDal = require('../components/users/usersDAL');
+const bcrypt = require('bcrypt');
 // const escapeHtml = require('escape-html');
 
 module.exports = function (passport) {
   passport.serializeUser(function (user, done) {
     console.log('serialize', user);
 
-    done(null, user.users_id);
+    done(null, user.user_id);
   });
 
   passport.deserializeUser(async function (id, done) {
     try {
-      const user = await userDal.getUser(id);
+      console.log('start serialize');
+      const user = await userDal.getUserById(id);
 
       console.log('de seiralize', user);
 
@@ -33,19 +36,49 @@ module.exports = function (passport) {
         passReqToCallback: true,
       },
       async function (req, email, password, done) {
-        console.log('email', email);
-        console.log('password', password);
-
         const user = new User(req.body);
 
         const userId = await userDal.add(user);
         user.users_id = userId;
 
         try {
-          console.log('success', user);
           return done(null, user);
         } catch (error) {
           return done(null, false, req.flash(error));
+        }
+      }
+    )
+  );
+  passport.use(
+    'login',
+    new LocalStrategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true,
+      },
+      async function (req, email, password, done) {
+        try {
+          const user = await userDal.getUserByEmail(email);
+
+          if (!user) {
+            return done(null, false, `User with email ${email} was not found`);
+            // return done(null, false, createError(401, `User with email ${email} was not found`));
+          }
+
+          const userHash = user.password;
+
+          const match = await bcrypt.compare(password, userHash);
+
+          const safeUser = service.createSafeUser(user);
+
+          if (!match) {
+            return done(null, false, 'Not a matching password');
+          }
+
+          return done(null, safeUser);
+        } catch (error) {
+          return done(error);
         }
       }
     )
