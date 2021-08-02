9ed59b03bcd62472de4d3efcdd7b63fd5192d0e1
Tester <TestTestTest1000@Test.com.ru>
2021-08-02 21:21:42 +0300

    add createSafeUser method

--- components/users/usersService.js
+++ components/users/usersService.js
@@ -1,3 +1,17 @@
-class UsersService {}
+class UsersService {
+  createSafeUser(user) {
+    const safeUserData = {
+      user_id: user.users_id,
+      data_create: user.data_create,
+      email: user.email,
+      surname: user.surname,
+      name: user.name,
+      patronymic: user.patronymic,
+      user_role: user.roles_id,
+    };
+
+    return safeUserData;
+  }
+}
 
 module.exports = new UsersService();
