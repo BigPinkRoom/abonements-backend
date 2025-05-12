733cff62c858df4c5a6528fbb062783a80d51a4b
NoName <newitmessages@gmail.com>
2025-05-12 22:17:03 +0300

    Added relative by id logic

--- components/relatives/relativesDAL.js
+++ components/relatives/relativesDAL.js
@@ -33,6 +33,31 @@ class RelativesModel {
       pool.releaseConnection(poolPromise);
     }
   }
+
+  async getRelativeById(relativeId) {
+    const sql = `SELECT r.*, c.telephone
+                 FROM mydb.relatives r
+                 LEFT JOIN mydb.telephone_numbers c ON r.relative_id = c.relative_id
+                 WHERE r.relative_id = ${relativeId}`;
+
+    let poolPromise = null;
+    try {
+      poolPromise = pool.promise();
+
+      const [rows, fields, error] = await poolPromise.execute(sql);
+      const result = rows;
+
+      console.log('result of getRelativeById', result);
+
+      if (error) throw error;
+      return result;
+    } catch (error) {
+      console.log('mysql error', error);
+      throw error;
+    } finally {
+      pool.releaseConnection(poolPromise);
+    }
+  }
 }
 
 module.exports = new RelativesModel();
